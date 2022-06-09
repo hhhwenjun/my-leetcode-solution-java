c9021553eb318cc6258bf2ae9fc4430b008c62e0
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-06-09 12:28:24 -0400

    Time: 2 ms (76.92%), Space: 43.9 MB (60.50%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> subsets = new ArrayList<>();
+        List<Integer> currentSubset = new ArrayList<>();
+
+        subsetsWithDupHelper(subsets, currentSubset, nums, 0);
+        return subsets;
+    }
+
+    private void subsetsWithDupHelper(List<List<Integer>> subsets, List<Integer> currentSubset, int[] nums, int index) {
+        // Add the subset formed so far to the subsets list.
+        subsets.add(new ArrayList<>(currentSubset));
+
+        for (int i = index; i < nums.length; i++) {
+            // If the current element is a duplicate, ignore.
+            if (i != index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            currentSubset.add(nums[i]);
+            subsetsWithDupHelper(subsets, currentSubset, nums, i + 1);
+            currentSubset.remove(currentSubset.size() - 1);
+        }
+    }
+}
