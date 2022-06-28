6607e58a479039efff20fca3875e16007a5ed1d8
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-06-28 12:36:54 -0400

    Time: 198 ms, Memory: 213.4 MB - LeetHub

--- find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,36 +1,35 @@
 class Solution {
-    // abstract: if there is a valid path from source to destination
-    // DFS: 1. Record each vertex and neighbours in list
-    // 2. Keep finding the node until traverse all of them
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> nodes = new ArrayList<>();
+        // bfs
+        // 1. build up neighbor list(adjacency list)
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
         for (int i = 0; i < n; i++){
-            nodes.add(new ArrayList<>());
+            adjList.put(i, new ArrayList<Integer>());
         }
-        // neighbour list
+        // 2. use edges to fill the list
         for (int[] edge : edges){
-            nodes.get(edge[0]).add(edge[1]);
-            nodes.get(edge[1]).add(edge[0]);
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
         }
+        
+        // 3. find path from source to destination, using queue structure
+        Deque<Integer> nodequeue = new ArrayDeque<>();
         boolean[] seen = new boolean[n];
+        nodequeue.add(source);
         
-        Stack<Integer> stack = new Stack<>();
-        stack.add(source);
-        
-        while(!stack.isEmpty()){
-            int current = stack.pop();
-            
-            if (current == destination){
-                return true;
-            }
-            if (seen[current]){
+        while (!nodequeue.isEmpty()){
+            int current = nodequeue.pollLast();
+            if (seen[current] == true){
                 continue;
             }
             seen[current] = true;
+            if (current == destination){
+                return true;
+            }
             
-            // add the neighbours of current to the stack
-            for (Integer neighbour : nodes.get(current)){
-                stack.add(neighbour);
+            List<Integer> neighbors = adjList.get(current);
+            for (Integer neighbor : neighbors){
+                nodequeue.add(neighbor);
             }
         }
         return false;
