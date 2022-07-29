4f8abdbbcf56c68e4d19a17b079fb571cd05aa24
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-07-29 11:47:52 -0400

    Time: 768 ms (17.71%), Space: 41.7 MB (81.57%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,31 +1,32 @@
 class Solution {
-    /*
-    * abstract: build expression, return all number of expressions
-    * Use all num in nums, reach the target sum
-    * backtracking(recursion)
-    */
-    int res;
     int[] nums;
+    int target;
+    int sum = 0;
     
     public int findTargetSumWays(int[] nums, int target) {
-        res = 0;
         this.nums = nums;
+        this.target = target;
+        
+        // 1. return # expressions, all possible solutions, backtracking
         backtracking(0, target);
-        return res;
+        return sum;
     }
-    
-    // backtracking process
+
+    // 2. recursion: input - index, output: sum
     public void backtracking(int index, int target){
-        // base case: reach the end of nums
-        if (index == nums.length && target == 0){
-            res++;
+        // 3. if the end result reach the target, return +1 to the output
+        // base case
+        if (target == 0 && index == nums.length){
+            sum++;
             return;
         }
-        if (index == nums.length){
+        else if (index == nums.length){
             return;
         }
-        // expressions building, can be +/-
-        backtracking(index + 1, target + nums[index]);
-        backtracking(index + 1, target - nums[index]); 
+        // if target > 0, continue
+        // for (int i = index; i < nums.length; i++){
+            backtracking(index + 1, target + nums[index]);
+            backtracking(index + 1, target - nums[index]);
+        // }
     }
 }
