1285594608852c0ddf24c9192217627b1fc7ba75
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-08-01 12:10:27 -0400

    Time: 148 ms (5.02%), Space: 69.9 MB (73.59%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,35 +1,42 @@
 class Solution {
-    public int[][] updateMatrix(int[][] mat) {
-        int r = mat.length, c = mat[0].length;
-        
-        int[][] dis = new int[r][c];
-        
-        for(int i = 0; i < r; i++)
-            for(int j = 0; j < c; j++)
-                if(mat[i][j] == 0) {
-                    explore(mat, dis, dis[i][j], i+1, j);
-                    explore(mat, dis, dis[i][j], i-1, j);
-                    explore(mat, dis, dis[i][j], i, j+1);
-                    explore(mat, dis, dis[i][j], i, j-1);
+    public int[][] updateMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int ones = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                if(matrix[i][j] == 1){
+                    matrix[i][j] = -1;
+                    ones++;
                 }
-        
-        return dis;
-    }
-    
-    public void explore(int[][] mat, int[][] dis, int wt, int i, int j) {
-        if(i < 0 || j < 0 || i >= mat.length || j >= mat[0].length || mat[i][j] != 1)
-            return;
-        
-        mat[i][j] = 2;
-        
-        if(dis[i][j] == 0 || wt+1 < dis[i][j]) {
-            dis[i][j] = wt+1;
-            explore(mat, dis, wt+1, i+1, j);
-            explore(mat, dis, wt+1, i-1, j);
-            explore(mat, dis, wt+1, i, j+1);
-            explore(mat, dis, wt+1, i, j-1);    
+        int x = 0;
+        while(ones > 0){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++){
+                    if(matrix[i][j] == x){
+                         if(isValid(matrix,i+1,j) && matrix[i+1][j] == -1){
+                             matrix[i+1][j] = x + 1;
+                             ones--;
+                         }
+                        if(isValid(matrix,i-1,j) && matrix[i-1][j] == -1){
+                             matrix[i-1][j] = x + 1;
+                             ones--;
+                         }
+                        if(isValid(matrix,i,j+1) && matrix[i][j+1] == -1){
+                             matrix[i][j+1] = x + 1;
+                             ones--;
+                         }if(isValid(matrix,i,j-1) && matrix[i][j-1] == -1){
+                             matrix[i][j-1] = x + 1;
+                             ones--;
+                         }
+                    }
+                }
+            }
+            x++;
         }
-        
-        mat[i][j] = 1;
+        return matrix;
+    }
+    private boolean isValid(int[][] mat, int i, int j){
+        return (i >= 0 && i < mat.length && j >= 0 && j < mat[0].length);
     }
 }
