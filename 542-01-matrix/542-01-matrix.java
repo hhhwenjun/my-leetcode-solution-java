5b66c0ce9a725612ec9ab04f17aa840d5267e11f
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-08-01 12:11:27 -0400

    Time: 22 ms (53.19%), Space: 71 MB (69.65%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,42 +1,34 @@
 class Solution {
     public int[][] updateMatrix(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int ones = 0;
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < m; j++)
-                if(matrix[i][j] == 1){
-                    matrix[i][j] = -1;
-                    ones++;
+        int m = matrix.length;
+        int n = matrix[0].length;
+       Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    queue.offer(new int[] {i, j});
                 }
-        int x = 0;
-        while(ones > 0){
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    if(matrix[i][j] == x){
-                         if(isValid(matrix,i+1,j) && matrix[i+1][j] == -1){
-                             matrix[i+1][j] = x + 1;
-                             ones--;
-                         }
-                        if(isValid(matrix,i-1,j) && matrix[i-1][j] == -1){
-                             matrix[i-1][j] = x + 1;
-                             ones--;
-                         }
-                        if(isValid(matrix,i,j+1) && matrix[i][j+1] == -1){
-                             matrix[i][j+1] = x + 1;
-                             ones--;
-                         }if(isValid(matrix,i,j-1) && matrix[i][j-1] == -1){
-                             matrix[i][j-1] = x + 1;
-                             ones--;
-                         }
-                    }
+                else {
+                    matrix[i][j] = Integer.MAX_VALUE;
                 }
             }
-            x++;
         }
+        // DO BFS IN ALL NEIGHBOURS SO THAT WE GET FINAL RESULT 
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] d : dirs) {
+                int r = cell[0] + d[0];
+                int c = cell[1] + d[1];
+				 // if new cell is out of bounds or is already closer to another 0, stop further bfs in this cell 
+                if (r < 0 || r >= m || c < 0 || c >= n || 
+                    matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) continue;
+                queue.add(new int[] {r, c});
+                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
+            }
+        }
+        
         return matrix;
-    }
-    private boolean isValid(int[][] mat, int i, int j){
-        return (i >= 0 && i < mat.length && j >= 0 && j < mat[0].length);
     }
 }
