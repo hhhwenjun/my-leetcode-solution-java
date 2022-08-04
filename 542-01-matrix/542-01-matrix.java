8a6cf91d422bffed3db76e75ffc39704f2c69ecc
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-08-04 11:36:56 -0400

    Time: 14 ms (72.76%), Space: 44.8 MB (94.06%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,34 +1,42 @@
 class Solution {
-    public int[][] updateMatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-       Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    queue.offer(new int[] {i, j});
-                }
-                else {
-                    matrix[i][j] = Integer.MAX_VALUE;
+    public int[][] updateMatrix(int[][] mat) {
+        // get the dimension info of the matrix
+        int length = mat.length;
+        int width = mat[0].length;
+        boolean[][] visited = new boolean[length][width];
+        Queue<int[]> queue = new LinkedList<>();
+        
+        // bfs: put cell to a queue
+        // 1. put all the 0 cells into a queue, mark visited array as true
+        for (int i = 0; i < length; i++){
+            for (int j = 0; j < width; j++){
+                if (mat[i][j] == 0){
+                    // dimension: i, j || distance
+                    queue.add(new int[]{i, j, 0});
+                    visited[i][j] = true;
                 }
             }
         }
-        // DO BFS IN ALL NEIGHBOURS SO THAT WE GET FINAL RESULT 
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            for (int[] d : dirs) {
-                int r = cell[0] + d[0];
-                int c = cell[1] + d[1];
-				 // if new cell is out of bounds or is already closer to another 0, stop further bfs in this cell 
-                if (r < 0 || r >= m || c < 0 || c >= n || 
-                    matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) continue;
-                queue.add(new int[] {r, c});
-                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
+        int[][] directions = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        // 2. poll each cell out, traverse 4 directions unvisited array, + 1
+        while(!queue.isEmpty()){
+            int[] curr = queue.poll();
+            int currX = curr[0];
+            int currY = curr[1];
+            int currDis = curr[2];
+            mat[currX][currY] = currDis;
+            
+            for (int[] direction : directions){
+                int nextX = currX + direction[0];
+                int nextY = currY + direction[1];
+                if (nextX < 0 || nextX >= length || nextY < 0 || nextY >= width || visited[nextX][nextY]){
+                    continue;
+                }
+                // 3. put them back to queue, repeat the process
+                queue.add(new int[]{nextX, nextY, currDis + 1});
+                visited[nextX][nextY] = true;
             }
         }
-        
-        return matrix;
+        return mat;
     }
 }
