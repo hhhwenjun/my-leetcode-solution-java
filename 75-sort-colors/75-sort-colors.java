02e5e5fc9eeef0ba2a83aff49e683bfee866552c
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-08-31 13:08:15 -0400

    Time: 1 ms (41.99%), Space: 42.5 MB (57.61%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,5 +1,6 @@
 class Solution {
-    public void sortColors(int[] nums) {
+   public void sortColors(int[] nums) {
+        // record the number of 0, 1, 2
         int zero = 0;
         int one = 0;
         int two = 0;
@@ -12,20 +13,67 @@ class Solution {
                 two++;
             }
         }
-        
-        for (int i = 0; i < nums.length; i++){
-            if (zero != 0){
-                nums[i] = 0;
-                zero--;
+
+        // three pointers point to start of 0, 1, 2 area in the array
+        int p0 = 0, p1 = zero, p2 = zero + one;
+        int p0End = zero - 1, p1End = zero + one - 1, p2End = nums.length - 1;
+        // first we clear the area of 1, make sure 1 area swap all 0 with 0 area
+            while(p0 < zero){
+            if(nums[p0] == 0){
+                p0++;
+                continue;
             }
-            else if (one != 0){
-                nums[i] = 1;
-                one--;
+            else if (nums[p0] == 1){
+                p1 = find(nums, 0, p1, p1End);
+                if (p1 == -1){
+                    p2 = find(nums, 0, p2, p2End);
+                    swap(nums, p0, p2);
+                } else {
+                    swap(nums, p0, p1);
+                }
             }
+            // p0 location is a 2
             else {
-                nums[i] = 2;
-                two--;
+                p2 = find(nums, 0, p2, p2End);
+                if (p2 == -1){
+                    p1 = find(nums, 0, p1, p1End);
+                    swap(nums, p0, p1);
+                }
+                else {
+                    swap(nums, p0, p2);
+                }
             }
         }
+        p1 = zero; p2 = zero + one;
+        while(p1 <= p1End && p2 <= p2End){
+            while(p1 <= p1End && nums[p1] == 1){
+                p1++;
+            }
+            while(p2 <= p2End && nums[p2] == 2){
+                p2++;
+            }
+            if (p1 <= p1End && p2 <= p2End){
+                swap(nums, p1, p2);
+                p1++; p2++;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public int find(int[] nums, int target, int currPointer, int endPointer){
+        if (currPointer == -1) return -1;
+        
+        while(currPointer <= endPointer){
+            if (nums[currPointer] == target){
+                return currPointer;
+            }
+            currPointer++;
+        }
+        return -1;
     }
 }
