f645491e4a33bf57bfb60ad6ef8d0f589a8a26cf
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-08-31 22:48:29 -0400

    Time: 106 ms (37.58%), Space: 124.2 MB (77.43%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,23 +1,86 @@
-class LRUCache extends LinkedHashMap<Integer, Integer>{
+class LRUCache {
+    
+    private LinkedNode head;
+    private LinkedNode tail;
+    private Map<Integer, LinkedNode> cache = new HashMap<>(); // key, node with val
+    private int size;
     private int capacity;
     
+    class LinkedNode {
+        int key;
+        int value;
+        LinkedNode prev;
+        LinkedNode next;
+    }
+    
+    public void add(LinkedNode node){
+        // add to the front
+        node.next = head.next;
+        node.prev = head;
+        
+        head.next = node;
+        node.next.prev = node;
+        
+    }
+    
+    public void remove(LinkedNode node){
+        // remove the node
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    
+    public LinkedNode removeTail(){
+        // remove from the tail
+        LinkedNode target = tail.prev;
+        remove(target);
+        return target;
+    }
+    
+    public void moveToFront(LinkedNode node){
+        remove(node);
+        add(node);
+    }
 
     public LRUCache(int capacity) {
-        super(capacity, 0.75F, true);
+        tail = new LinkedNode();
+        head = new LinkedNode();
+        tail.prev = head;
+        head.next = tail;
+        
         this.capacity = capacity;
+        size = 0;
     }
     
     public int get(int key) {
-        return super.getOrDefault(key, -1);
+        LinkedNode target = cache.get(key);
+        if (target == null) return -1;
+        moveToFront(target);
+        return target.value;
     }
     
     public void put(int key, int value) {
-        super.put(key, value);
-    }
-    
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest){
-        return size() > capacity;
+        LinkedNode curr = cache.get(key);
+        
+        if (curr == null){
+            // create new node
+            LinkedNode newNode = new LinkedNode();
+            newNode.value = value;
+            newNode.key = key;
+            cache.put(key, newNode);
+            add(newNode);
+            size++;
+            
+            if (size > capacity){
+                LinkedNode removeNode = removeTail();
+                cache.remove(removeNode.key);
+                size--;
+            }
+        }
+        else {
+            // update the value
+            curr.value = value;
+            moveToFront(curr);
+        }
     }
 }
 
