c310e119b82492c39f5249a0e71a09bb2e64b99e
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-09-05 12:13:18 -0400

    Time: 3 ms (41.39%), Space: 41.3 MB (83.47%) - LeetHub

--- 593-valid-square/593-valid-square.java
+++ 593-valid-square/593-valid-square.java
@@ -1,28 +1,16 @@
 class Solution {
     public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
-        // best way: find diagonal and check if two diagonal line equals
-        // use p1, to find diagonal
+        Set<Integer> squarelength = new HashSet<>();
+        // calculate all distances between 4 points
         int[][] points = new int[][]{p1, p2, p3, p4};
-        int squareLength1 = 0; // can be side length, or diagonal
-        int squareLength2 = 0; // can be side length, or diagonal
-        
-        for (int i = 0; i < 4; i++){
-            for (int j = i + 1; j < 4; j++){
-                int countLength = (points[i][0] - points[j][0]) * (points[i][0] - points[j][0])
-                    + (points[i][1] - points[j][1]) * (points[i][1] - points[j][1]);
-                if (countLength == 0) return false; // duplicate point
-                if (squareLength1 == 0){
-                    squareLength1 = countLength;
-                }
-                else if (squareLength2 == 0 && countLength != squareLength1){
-                    squareLength2 = countLength;
-                }
-                else {
-                    // check if current length is either side or diagonal
-                    if (countLength != squareLength1 && countLength != squareLength2) return false;
-                }
+        for (int i = 0; i < points.length; i++){
+            for (int j = i + 1; j < points.length; j++){
+                int distance = (int)Math.pow(points[i][0] - points[j][0], 2) 
+                    + (int)Math.pow(points[i][1] - points[j][1], 2);
+                if (distance == 0) return false; // cannot be at the same location
+                squarelength.add(distance);
             }
         }
-        return true;
+        return squarelength.size() == 2;
     }
 }
