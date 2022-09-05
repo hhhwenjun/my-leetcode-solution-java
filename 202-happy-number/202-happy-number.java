c9297fcc4dc59a858c35014794e3c5ab5412f9a9
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-09-05 10:43:49 -0400

    Time: 2 ms (67.83%), Space: 40.7 MB (82.26%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean isHappy(int n) {
-        // loops in a cycle: use hashset
-        Set<Integer> set = new HashSet<>();
-        int curr = n;
-        while(curr != 1){
-            if (set.contains(curr)) return false;
-            set.add(curr);
-            // calculate the sum of the squares
-            int sum = 0;
-            while(curr > 0){
-                int digit = curr % 10;
-                curr /= 10;
-                sum += digit * digit;
-            }
-            curr = sum;
+        int slow = n, fast = next(n);
+        while(n != 1 && slow != fast){
+            slow = next(slow);
+            fast = next(next(fast));
         }
-        return true;
+        return slow == 1;
+    }
+    
+    public int next(int n){
+        int curr = n;
+        int sum = 0;
+        while(curr > 0){
+            int digit = curr % 10;
+            curr /= 10;
+            sum += digit * digit;
+        }
+        return sum;
     }
 }
