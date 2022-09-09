14dc15ba4e35405f4ca39778c20e68cf7580836a
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-09-09 11:38:51 -0400

    Time: 5 ms (58.98%), Space: 73.7 MB (65.87%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,18 +1,11 @@
 class Solution {
     public int maxArea(int[] height) {
         // always looking for highest two lines to contain water
-        int left = 0, right = height.length - 1;
-        int maxWater = 0;
-        while(left <= right){
-            maxWater = Math.max(maxWater, 
-                                Math.min(height[left], height[right]) * (right - left));
-            if (height[left] < height[right]){
-                left++;
-            }
-            else {
-                right--;
-            }
+        int max = 0;
+        for (int i = 0, j= height.length - 1; i < j; ) {
+            int minHeight = height[i] < height[j] ? height[i++] : height[j--];
+            max = Math.max(max, (j - i + 1) * minHeight);
         }
-        return maxWater;
+        return max;
     }
 }
