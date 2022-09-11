f922512f8577ce9f26c3b4d53486778fde45b957
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-09-11 09:38:04 -0400

    Time: 2 ms (60.00%), Space: 49 MB (40.86%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,24 +1,19 @@
 class Solution {
     public int trap(int[] height) {
-        // two pointers： low, high
-        int left = 0, right = height.length - 1;
-        int maxLeft = 0, maxRight = 0;
+        // two pointers: left, right, maxLeft, maxRight
+        int left = 0, right = height.length - 1, maxLeft = 0, maxRight = 0;
         int water = 0;
         
         while(left < right){
-            // current right pointer is max right
-            // 1. compare two pointer, always keep the high one
             if (height[left] < height[right]){
-                if (height[left] >= maxLeft){
+                if (height[left] > maxLeft){
                     maxLeft = height[left];
-                } 
+                }
                 else water += maxLeft - height[left];
-                // 2. move the lower pointer, record left and right max
                 left++;
             }
             else {
-                 // 3. when move the lower one, record max - low value
-                if (height[right] >= maxRight){
+                if (height[right] > maxRight){
                     maxRight = height[right];
                 }
                 else water += maxRight - height[right];
@@ -26,7 +21,5 @@ class Solution {
             }
         }
         return water;
-        
-       
     }
 }
