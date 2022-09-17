e81810f447bcd7debda3a2f475d1de9146684485
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-09-17 10:37:47 -0400

    Time: 3 ms (49.59%), Space: 44.7 MB (9.56%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,25 +1,31 @@
 class Solution {
+    List<List<Integer>> res;
+    Set<List<Integer>> findDuplicate;
+    int[] nums;
+    
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        // backtracking: then use a set to find the duplicates
         Arrays.sort(nums);
-        List<List<Integer>> subsets = new ArrayList<>();
-        List<Integer> currentSubset = new ArrayList<>();
-
-        subsetsWithDupHelper(subsets, currentSubset, nums, 0);
-        return subsets;
+        this.nums = nums;
+        res = new ArrayList<>();
+        findDuplicate = new HashSet<>();
+        for (int i = 0; i < nums.length + 1; i++){
+            backtracking(0, i, new ArrayList<Integer>());
+        }
+        return res;
     }
-
-    private void subsetsWithDupHelper(List<List<Integer>> subsets, List<Integer> currentSubset, int[] nums, int index) {
-        // Add the subset formed so far to the subsets list.
-        subsets.add(new ArrayList<>(currentSubset));
-
-        for (int i = index; i < nums.length; i++) {
-            // If the current element is a duplicate, ignore.
-            if (i != index && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            currentSubset.add(nums[i]);
-            subsetsWithDupHelper(subsets, currentSubset, nums, i + 1);
-            currentSubset.remove(currentSubset.size() - 1);
+    
+    public void backtracking(int index, int length, List<Integer> subset){
+        if (subset.size() == length){
+            res.add(new ArrayList(subset));
+        }
+        
+        // backtracking
+        for (int i = index; i < nums.length; i++){
+            if (i != index && nums[i] == nums[i - 1]) continue;
+            subset.add(nums[i]);
+            backtracking(i + 1, length, subset);
+            subset.remove(subset.size() - 1);
         }
     }
 }
