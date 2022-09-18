3afc66f9939b3cf76b3c8904c972d99761587940
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-09-18 12:21:11 -0400

    Time: 3 ms (48.08%), Space: 45.1 MB (24.29%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,35 +1,24 @@
 class Solution {
-    /**
-    * abstract: return all permutations, unique number, return in list
-    * backtracking algorithm(recursion) 
-    */
     List<List<Integer>> res;
     List<Integer> nums;
-    int length;
     
     public List<List<Integer>> permute(int[] nums) {
-        this.nums = new ArrayList<>();
+        this.nums = new ArrayList<Integer>();
+        for (int num : nums) this.nums.add(num);
         res = new ArrayList<>();
-        for (int num : nums){
-            this.nums.add(num);
-        }
-        length = nums.length;
         backtracking(0);
         return res;
     }
     
-    // backtracking process realization
-    public void backtracking(int start){
-        // base case: reach the end of given nums
-        if (start == length){
-            // add the current permutation to result list
+    public void backtracking(int index){
+        if (index == nums.size()){
             res.add(new ArrayList(nums));
-            return;
         }
-        for (int i = start; i < length; i++){
-            Collections.swap(nums, start, i);
-            backtracking(start + 1);
-            Collections.swap(nums, start, i);
-        }    
+        // backtracking process
+        for (int i = index; i < nums.size(); i++){
+            Collections.swap(nums, i, index);
+            backtracking(index + 1);
+            Collections.swap(nums, i, index);
+        }
     }
 }
