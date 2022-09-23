5d397fd31e5d6e800849e4a29f7691c53a96725d
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-09-22 20:46:25 -0400

    Time: 3 ms (77.23%), Space: 43.7 MB (13.79%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,60 +1,60 @@
 class Solution {
-    // bfs: each minute we regard it as a level
-    // 4 directions
-    int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    // dfs: traverse all the nearby oranges
+    public int[][] directions = new int[][]{{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    int[][] grid;
+    Queue<int[]> queue;
+    int length;
+    int width;
     
     public int orangesRotting(int[][] grid) {
-        Queue<int[]> level = new LinkedList<>();
-        // find all rotten orange in the current grid
-        int m = grid.length;
-        int n = grid[0].length;
+        this.grid = grid;
+        int time = 0;
+        queue = new LinkedList<>();
+        length = grid.length;
+        width = grid[0].length;
         
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
+        // find all the rotten oranges in plot
+        for (int i = 0; i < length; i++){
+            for (int j = 0; j < width; j++){
                 if (grid[i][j] == 2){
-                    level.add(new int[]{0, i, j});
+                    queue.add(new int[]{0, i, j});
                 }
             }
         }
-        // use queue to store level-orange, put the neighbors to queue
-        int min = 0;
-        while (!level.isEmpty()){
-            int[] current = level.poll();
-            int currentLevel = current[0];
-            min = Math.max(min, currentLevel);
-            List<int[]> neighbors = findNeighbors(new int[]{current[1], current[2]}, m - 1, n - 1, grid);
-            if (neighbors.size() == 0) continue;
-            for (int[] neighbor : neighbors){
-                level.add(new int[]{currentLevel + 1, neighbor[0], neighbor[1]});
-                grid[neighbor[0]][neighbor[1]] = 2;
-            }
+        
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int currLevel = current[0];
+            time = Math.max(time, currLevel);
+            findOther(currLevel, current[1], current[2]);
         }
-        // finally check if the grid has 1 or not(if so, return -1)
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
+        
+        // find if there is any 1 inside the grid
+        for (int i = 0; i < length; i++){
+            for (int j = 0; j < width; j++){
                 if (grid[i][j] == 1){
                     return -1;
                 }
             }
         }
-        return min;
+        
+        return time;
     }
     
-    public List<int[]> findNeighbors(int[] current, int maxRow, int maxCol, int[][] grid){
-        List<int[]> neighbors = new ArrayList<int[]>();
-        int currentRow = current[0];
-        int currentCol = current[1];
-        
+    // dfs find other rotten oranges for next time
+    public void findOther(int currLevel, int row, int col){
         for (int[] direction : directions){
-            int neighborRow = currentRow + direction[0];
-            int neighborCol = currentCol + direction[1];
+            int nextRow = row + direction[0];
+            int nextCol = col + direction[1];
             
-            if (neighborRow < 0 || neighborRow > maxRow || neighborCol < 0 
-                || neighborCol > maxCol || grid[neighborRow][neighborCol] != 1){
+            if (nextRow < 0 || nextRow >= length 
+                || nextCol < 0 || nextCol >= width 
+                || grid[nextRow][nextCol] != 1){
                 continue;
             }
-            neighbors.add(new int[]{neighborRow, neighborCol});
+            
+            grid[nextRow][nextCol] = 2;
+            queue.add(new int[]{currLevel + 1, nextRow, nextCol});
         }
-        return neighbors;
     }
 }
