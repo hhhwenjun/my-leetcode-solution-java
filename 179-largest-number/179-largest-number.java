3dd460c5122a3f6bc72bf1e89e4cde20d8128d12
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-09-27 11:29:15 -0400

    Time: 6 ms (89.44%), Space: 41.8 MB (94.52%) - LeetHub

--- 179-largest-number/179-largest-number.java
+++ 179-largest-number/179-largest-number.java
@@ -1,33 +1,24 @@
 class Solution {
     
-    private class LargeNumberComparator implements Comparator<String>{
-        @Override
-        public int compare(String a, String b){
-            String order1 = a + b;
-            String order2 = b + a;
-            return order2.compareTo(order1);
-        }
-    }
-    
-    public String largestNumber(int[] nums) {
-        // get input integers as strings
+  public String largestNumber(int[] nums) {
+        if (nums == null || nums.length == 0) return "";
         String[] strs = new String[nums.length];
-        for (int i = 0; i < nums.length; i++){
-            strs[i] = String.valueOf(nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = nums[i]+"";
         }
-        
-        // sort strings according to custom comparator
-        Arrays.sort(strs, new LargeNumberComparator());
-        
-        // if, after being sorted, the largest number is 0,
-        // the entire number is 0.
-        if (strs[0].equals("0")) return "0";
-        
-        // build largest number from sorted array
-        String largestNumberStr = new String();
-        for (String numStr : strs){
-            largestNumberStr += numStr;
+        Arrays.sort(strs, new Comparator<String>() {
+            @Override
+            public int compare(String i, String j) {
+                String s1 = i+j;
+                String s2 = j+i;
+                return s1.compareTo(s2);
+            }
+        });
+        if (strs[strs.length-1].charAt(0) == '0') return "0";
+        String res = new String();
+        for (int i = 0; i < strs.length; i++) {
+            res = strs[i]+res;
         }
-        return largestNumberStr;
+        return res;
     }
 }
