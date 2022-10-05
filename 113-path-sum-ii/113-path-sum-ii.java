923da0d76edb8e216a53ba0f56be63de751be265
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-10-05 12:07:33 -0400

    Time: 2 ms (85.24%), Space: 44.8 MB (36.18%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -14,37 +14,28 @@
  * }
  */
 class Solution {
-    List<List<Integer>> res;  
+    List<List<Integer>> res;
     
-    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {    
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         res = new ArrayList<>();
-        if (root == null){
-            return res;
-        }
-        
-        backtracking(root, new ArrayList<>(), targetSum);
+        if (root == null) return res;
+        backtracking(root, new ArrayList<Integer>(), targetSum);
         return res;
     }
     
-    // backtracking to traverse the tree and find all solutions
-    public void backtracking(TreeNode root, List<Integer> subset, int numLeft){
-        if (root == null){
-            return;
-        }
-        subset.add(root.val);
+    // use backtracking method to find the route and put in the list
+    public void backtracking(TreeNode root, List<Integer> subset, int sum){
+        if (root == null) return;
         
-        if (root.val == numLeft && root.left == null && root.right == null){
+        subset.add(root.val);
+        if (sum == root.val && root.left == null && root.right == null){
             res.add(new ArrayList(subset));
-            //return;
         }
         else {
-            backtracking(root.left, subset, numLeft - root.val);
-            backtracking(root.right, subset, numLeft - root.val);
+            backtracking(root.left, subset, sum - root.val);
+            backtracking(root.right, subset, sum - root.val);
         }
-        // backtracking process
-        // subset.add(root.val);
-        
         subset.remove(subset.size() - 1);
-        numLeft += root.val;
+        sum += root.val;
     }
 }
