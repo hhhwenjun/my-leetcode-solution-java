ef6c3888a73980ce62328b42f3c0f27fdb65b2a8
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-10-07 22:35:28 -0400

    Time: 21 ms (8.63%), Space: 45.2 MB (36.26%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.java
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -1,11 +1,7 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        // sort the intervals by the starting time of meetings
-        // each time check the end time vs. next starting time of meeting
-        
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         int prev = 0;
-        
         for (int[] interval : intervals){
             int start = interval[0];
             int end = interval[1];
