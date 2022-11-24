1700afc8f20ae0d30cd7bc284fffa65e55c8e787
Wenjun H <66753253+hhhwenjun@users.noreply.github.com>
2022-11-24 11:30:23 -0500

    Time: 57 ms (5.06%), Space: 54.9 MB (73.47%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,19 +5,15 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         
         // 2. create a list to use it as a stack
-        List<int[]> stack = new LinkedList<>();
+        LinkedList<int[]> stack = new LinkedList<>();
         
         for (int[] interval : intervals){
-            if (stack.isEmpty() || stack.get(stack.size() - 1)[1] < interval[0]){
+            if (stack.isEmpty() || stack.getLast()[1] < interval[0]){
                 stack.add(interval);
                 continue;
             }
             // merge the interval
-            int[] lastInterval = stack.get(stack.size() - 1);
-            stack.remove(stack.size() - 1);
-            interval[0] = Math.min(interval[0], lastInterval[0]);
-            interval[1] = Math.max(interval[1], lastInterval[1]);
-            stack.add(interval);
+            stack.getLast()[1] = Math.max(stack.getLast()[1], interval[1]);
         }
         int[][] res = new int[stack.size()][2];
         for (int i = 0; i < stack.size(); i++){
